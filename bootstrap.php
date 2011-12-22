7deca764a45652ddf4c3db0637a25539f24ec9dd
Julien Brochet <mewt@madalynn.eu>
2011-12-22 21:34:06 +0100

    added Plum support

--- bootstrap.php
+++ bootstrap.php
@@ -13,10 +13,15 @@ require_once __DIR__.'/vendor/silex.phar';
 
 $app = new Silex\Application();
 
+// Configuration file
+require_once __DIR__.'/config.php';
+
 $app['autoloader']->registerNamespaces(array(
     'ZeldaLogs'     => __DIR__.'/src',
     'Madalynn\Twig' => __DIR__.'/vendor/irc-parser-extension/src',
-    'Symfony'       => __DIR__.'/vendor'
+    'Symfony'       => __DIR__.'/vendor',
+    'Plum\Silex'    => __DIR__.'/vendor/plum-service-provider/src',
+    'Plum'          => __DIR__.'/vendor/plum/src'
 ));
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
@@ -27,6 +32,20 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 $app->register(new ZeldaLogs\Silex\Provider\ZeldaLogsServiceProvider(), array(
     'zeldalogs.prefix'          => 'zelda.log.',
     'zeldalogs.date.format'     => 'dMY',
-    'zeldalogs.directory'       => __DIR__.'/logs',
+    'zeldalogs.directory'       => $config['zeldalogs.directory'],
     'zeldalogs.number.of.lines' => 300
+));
+
+$app->register(new Plum\Silex\Provider\PlumServiceProvider(), array(
+    'plum.servers' => array(
+        'bender' => array(
+            'host'    => 'bender.madalynn.eu',
+            'user'    => 'web',
+            'dir'     => '/home/web/madalynn.eu/zelda',
+            'port'    => 2222,
+            'options' => array(
+                'rsync_exclude' => __DIR__.'/rsync_exclude.txt'
+            )
+        )
+    )
 ));
---
+++ deploy.php
@@ -0,0 +1,15 @@
+#!/usr/bin/env php
+<?php
+
+/*
+ * This file is part of the ZeldaLogs website.
+ *
+ * (c) 2010-2011 Julien Brochet <mewt@madalynn.eu>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once 'bootstrap.php';
+
+$app['plum.deploy']('bender', 'rsync');
